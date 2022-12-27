0d0b32f17552720d7bee64a61cdbde9bca5ac4fa
rehmanshams <rehman.alphasquad@gmail.com>
2022-12-27 21:13:30 +0500

    fix the background of the popup

--- components/model.js
+++ components/model.js
@@ -9,7 +9,35 @@ export default function model({ setCardmodel, getid }) {
         role="dialog"
         aria-modal="true"
       >
-        <div className="fixed inset-0 bg-white bg-opacity-70 transition-opacity"></div>
+        <div
+          className={`${
+            getid?.skinMeta.find((e) => e.key == "rarity").value == "DELUXE" &&
+            getid.category == "Anime"
+              ? "bg-pink-200 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "PREMIUM" && getid.category == "Anime"
+              ? "bg-red-300 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "ULTRA" && getid.category == "Anime"
+              ? "bg-green-200 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "LEGENDARY" && getid.category == "Movies & TV"
+              ? "bg-red-300 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "LEGENDARY" && getid.category == "Nature"
+              ? "bg-yellow-200 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "ULTRA" && getid.category == "Nature"
+              ? "bg-pink-300 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "DELUXE" && getid.category == "Nature"
+              ? "bg-red-300 fixed inset-0  bg-opacity-70 transition-opacity"
+              : getid?.skinMeta.find((e) => e.key == "rarity").value ==
+                  "STANDARD" 
+              ? "bg-blue-300 fixed inset-0  bg-opacity-70 transition-opacity"
+              : "vkldnkj"
+          }`}
+        ></div>
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center  ">
             <div className="w-full max-w-[920px] transform overflow-hidden rounded-lg bg-white shadow-xl transition-all ">
@@ -83,7 +111,54 @@ export default function model({ setCardmodel, getid }) {
                   <div className="mb-6 mt-6">
                     <button
                       onClick={() => setCardmodel(false)}
-                      className="border-2 text-2xl w-32 h-12 border-black font-thin rounded-md text-black"
+                      className={`
+                        ${
+                          getid?.skinMeta.find(
+                            (newitem) => newitem.key == "rarity"
+                          ).value == "DELUXE" && getid.category == "Anime"
+                            ? "border-blue-400 text-blue-400"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "PREMIUM" && getid.category == "Anime"
+                            ? "border-red-600 text-red-600"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "ULTRA" && getid.category == "Anime"
+                            ? "border-green-500 text-green-500"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "LEGENDARY" &&
+                              getid.category == "Movies & TV"
+                            ? "border-red-600 text-red-600"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "STANDARD"
+                            ? "border-blue-500 text-blue-500"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "ULTRA" &&
+                              getid.category == "Movies & TV"
+                            ? "border-blue-600 text-blue-600"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "LEGENDARY" &&
+                              getid.category == "Nature"
+                            ? "border-yellow-600 text-yellow-600"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "PREMIUM" && getid.category == "Nature"
+                            ? "border-yellow-600 text-yellow-600"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "ULTRA" && getid.category == "Nature"
+                            ? "border-pink-400 text-pink-400"
+                            : getid?.skinMeta.find(
+                                (newitem) => newitem.key == "rarity"
+                              ).value == "DELUXE" && getid.category == "Nature"
+                            ? "border-red-600 text-red-600"
+                            : "udjnvjdb"
+                        }
+                        border-2 text-2xl w-32 h-12  font-thin rounded-md `}
                     >
                       Canel
                     </button>
